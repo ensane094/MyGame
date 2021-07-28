5c8cf82cc4111cfb6b53d5e32572ff7abad99bbb
[ensane] <[ensane094@gmail.com]>
2021-07-28 16:01:50 +0300

    basic version

---
+++ android/src/gb/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package gb.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import gb.mygdx.game.MyFirstGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyFirstGame(), config);
+	}
+}
---
+++ desktop/src/gb/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gb.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gb.mygdx.game.MyFirstGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyFirstGame(), config);
+	}
+}
